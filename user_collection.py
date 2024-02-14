ee6a2515e2df79ad2d4e38eed91a8902cff9f348
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2024-02-15 09:57:54 +1000

    Test commit

--- user_collection.py
+++ user_collection.py
@@ -3,9 +3,12 @@ UserCollection class that stores user data in a list.
 """
 
 
-class UseCollection:
+class UserCollection:
     def __init__(self):
         self.users = []
 
     def __str__(self):
         return str(self.users)
+
+    def add_user(self, user):
+        self.users.append(user)
